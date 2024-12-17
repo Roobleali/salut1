f81107a961044f2002797f91ee274e575440ea6f
abdishakuuralim <37404316-abdishakuuralim@users.noreply.replit.com>
2024-12-17 21:09:59 +0000

    Agent query: Is the application running without any translation-related errors now?
    Refactor translation handling to use react-i18next directly.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/bade8796-7cc5-4eac-b9af-88a38361e58e/f136c4a0-f285-4b7f-87d3-58ba631cf555.jpg

--- client/src/hooks/use-translation.ts
+++ client/src/hooks/use-translation.ts
@@ -1,3 +1,5 @@
 // This file is deprecated.
 // Use react-i18next's useTranslation hook directly:
 // import { useTranslation } from 'react-i18next';
+
+export { useTranslation } from 'react-i18next';
